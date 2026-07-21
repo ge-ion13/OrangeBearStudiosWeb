b9530d577fd90eefb599f11c0c1de3c92f661117
ge-ion13 <giorgosi_13@outlook.com>
2026-07-21 19:28:31 +0300

    Improve SEO with canonical URLs, robots.txt and sitemap

--- src/main/kotlin/com/orangebearstudios/web/controller/WebController.kt
+++ src/main/kotlin/com/orangebearstudios/web/controller/WebController.kt
@@ -1,26 +1,39 @@
 package com.orangebearstudios.web.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class WebController {
 
     @GetMapping("/")
-    fun home(): String = "index"
-
-    @GetMapping("/about-us")
-    fun aboutUs(): String = "about-us"
-
-    @GetMapping("/apps/ridecare")
-    fun ridecare(): String = "ridecare"
-
-    @GetMapping("/games/thinkfast")
-    fun thinkfast(): String = "thinkfast"
-
-    @GetMapping("/privacy/thinkfast")
-    fun thinkFastPrivacy(): String {
-        return "privacy/thinkfast"
+    fun home(model: Model): String {
+        model.addAttribute("canonicalUrl", "https://orangebearstudios.com/")
+        return "index"
     }
 
+    @GetMapping("/about-us")
+    fun aboutUs(model: Model): String {
+        model.addAttribute("canonicalUrl", "https://orangebearstudios.com/about-us")
+        return "about-us"
+    }
+
+    @GetMapping("/apps/ridecare")
+    fun ridecare(model: Model): String {
+        model.addAttribute("canonicalUrl", "https://orangebearstudios.com/apps/ridecare")
+        return "ridecare"
+    }
+
+    @GetMapping("/games/thinkfast")
+    fun thinkfast(model: Model): String {
+        model.addAttribute("canonicalUrl", "https://orangebearstudios.com/games/thinkfast")
+        return "thinkfast"
+    }
+
+    @GetMapping("/privacy/thinkfast")
+    fun thinkFastPrivacy(model: Model): String {
+        model.addAttribute("canonicalUrl", "https://orangebearstudios.com/privacy/thinkfast")
+        return "privacy/thinkfast"
+    }
 }
